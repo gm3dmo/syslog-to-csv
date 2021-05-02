eee16338ea1402dde5ff25f4d91eeba35eb6fba2
David Morris YYY <gm3dmo@github.com>
2021-05-02 10:42:04 +0100

    Adding black format.

--- kv-to-csv.py
+++ kv-to-csv.py
@@ -2,9 +2,7 @@
 
 __version__ = "0.1.0"
 
-import os
 import csv
-import sys
 import json
 import logging
 import argparse
@@ -20,7 +18,7 @@ logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 @log2csv.timeit
 def main(args):
     # create logger
-    logger = logging.getLogger('root')
+    logger = logging.getLogger("root")
     FORMAT = "[%(filename)s:%(lineno)s - %(funcName)20s() ] %(message)s"
     logging.basicConfig(format=FORMAT)
     logger.setLevel(args.loglevel)
@@ -30,10 +28,9 @@ def main(args):
     logger.info(f"""filename: {args.filename}""")
     logger.info(f"""filename.stem: {filename.stem}""")
 
-    logfile_type = filename.stem.split('.')[0]
+    logfile_type = filename.stem.split(".")[0]
 
-    wanted='core'
-
+    wanted = "core"
 
     status_codes = log2csv.get_wanted_kv_headers(logtype=logfile_type)
     logger.debug(f"""status_codes: {status_codes}""")
@@ -43,9 +40,8 @@ def main(args):
     fieldnames.append("line_length")
 
     skipped_count = 0
-    with open(args.csv_file,"w") as csvfile:
-        writer  = csv.DictWriter(csvfile, delimiter=',',
-                fieldnames=fieldnames)
+    with open(args.csv_file, "w") as csvfile:
+        writer = csv.DictWriter(csvfile, delimiter=",", fieldnames=fieldnames)
         if args.header == "yes":
             writer.writeheader()
 
@@ -55,48 +51,63 @@ def main(args):
                 # Cheat section where new lines are removed
                 # and comma replaced with underscore.
                 try:
-                    line = str(line.replace(b'\r', b'~'), 'utf-8')
-                    line = str(line.replace(',', '_'))
+                    line = str(line.replace(b"\r", b"~"), "utf-8")
+                    line = str(line.replace(",", "_"))
                 except:
-                    logger.error(f"""Could not parse line number: {line_count} data: {line}""")
+                    logger.error(
+                        f"""Could not parse line number: {line_count} data: {line}"""
+                    )
                     continue
 
-                if wanted == 'core':
+                if wanted == "core":
                     # delete the keys of parsed line we don't want
                     try:
-                        raw_parsed_line = log2csv.parse_kv_pairs_two(line.rstrip('\r\n'))
-                        parsed_line = {k: raw_parsed_line[k] for k in status_codes[logfile_type][wanted] if k in raw_parsed_line }
-                        # line_count starts at zero. Add 1 to get 
+                        raw_parsed_line = log2csv.parse_kv_pairs_two(
+                            line.rstrip("\r\n")
+                        )
+                        parsed_line = {
+                            k: raw_parsed_line[k]
+                            for k in status_codes[logfile_type][wanted]
+                            if k in raw_parsed_line
+                        }
+                        # line_count starts at zero. Add 1 to get
                         # line number in the file.
                         parsed_line["line_number"] = line_count + 1
                         parsed_line["line_length"] = length_of_line
                     except:
-                        logger.error(f"""Could not parse line number: {line_count} data: {line}""")
-                        continue 
+                        logger.error(
+                            f"""Could not parse line number: {line_count} data: {line}"""
+                        )
+                        continue
                     if parsed_line != {}:
                         logger.debug(f"""line: {line_count}""")
                         logger.debug(f"""PLK: {parsed_line.keys()}""")
-                        if status_codes[logfile_type]['must'] not in parsed_line.keys():
+                        if status_codes[logfile_type]["must"] not in parsed_line.keys():
                             skipped_count += 1
-                            logger.debug(f""" i skipped line: {line_count} because: {parsed_line.keys()} does not contain: {status_codes[logfile_type]['must']} """)
+                            logger.debug(
+                                f""" i skipped line: {line_count} because: {parsed_line.keys()} does not contain: {status_codes[logfile_type]['must']} """
+                            )
                             continue
                         else:
                             pass
                         logger.debug(f"""PLK2: {parsed_line.keys()}""")
                         writer.writerow(parsed_line)
                     else:
-                       logger.debug(f"""line failed to parse: {line_count}""")
-                       logger.debug(f"""{line}""")
-                       next
+                        logger.debug(f"""line failed to parse: {line_count}""")
+                        logger.debug(f"""{line}""")
+                        next
 
     csv_size_in_bytes = os.stat(csvfile.name).st_size
     human_size_of_csv = log2csv.sizer(csv_size_in_bytes)
 
-    logger.info(f"""Converted file: {filename} size type: {logfile_type} to CSV file {csvfile.name} size {csv_size_in_bytes} bytes or roughly {human_size_of_csv}.""")
+    logger.info(
+        f"""Converted file: {filename} size type: {logfile_type} to CSV file {csvfile.name} size {csv_size_in_bytes} bytes or roughly {human_size_of_csv}."""
+    )
     logger.info(f"""Skipped={skipped_count} Lines in file={line_count} lines.""")
 
+
 if __name__ == "__main__":
-    """ This is executed when run from the command line """
+    """This is executed when run from the command line"""
     parser = argparse.ArgumentParser()
 
     parser.add_argument("filename", help="a syslog file")
