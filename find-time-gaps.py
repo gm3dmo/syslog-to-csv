6de109c56d3239a2f6b209d4838c3d52a54c2c26
David Morris <gm3dmo@github.com>
2021-08-07 15:32:34 +0100

    Format with black to make linter happy.

--- find-time-gaps.py
+++ find-time-gaps.py
@@ -16,67 +16,75 @@ import pandas as pd
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
+
 def main(args):
 
-   logger = logging.getLogger("find-time-gaps")
-   logger.setLevel(args.loglevel)
+    logger = logging.getLogger("find-time-gaps")
+    logger.setLevel(args.loglevel)
 
-   logger.info(f"""reading {args.filename}""")
-   logger.info(f"""acting on {args.column_name}""")
-   logger.info(f"""search for gaps of {args.gap}""")
-   df = pd.read_csv(args.filename)
-   logger.debug(f"""\n{df.head()}""")
+    logger.info(f"""reading {args.filename}""")
+    logger.info(f"""acting on {args.column_name}""")
+    logger.info(f"""search for gaps of {args.gap}""")
+    df = pd.read_csv(args.filename)
+    logger.debug(f"""\n{df.head()}""")
 
-   seconds_per_day = 86400
-   values_to_eliminate = seconds_per_day - args.tolerance
+    seconds_per_day = 86400
+    values_to_eliminate = seconds_per_day - args.tolerance
 
-   df[args.column_name] = pd.to_datetime(df[args.column_name], errors='coerce')
-   df.dropna(inplace=True, subset=[args.column_name])
-   logger.debug(df[args.column_name])
-   df['time_difference'] = df[args.column_name].diff().dt.total_seconds()
-   df['longer_gap'] = (df[args.column_name].diff()).dt.total_seconds() >= args.gap
+    df[args.column_name] = pd.to_datetime(df[args.column_name], errors="coerce")
+    df.dropna(inplace=True, subset=[args.column_name])
+    logger.debug(df[args.column_name])
+    df["time_difference"] = df[args.column_name].diff().dt.total_seconds()
+    df["longer_gap"] = (df[args.column_name].diff()).dt.total_seconds() >= args.gap
 
-   logger.info("** This section contains locations of detected gaps:***")
-   logger.info(f"""*** These values will be removed {values_to_eliminate} ***""")
+    logger.info("** This section contains locations of detected gaps:***")
+    logger.info(f"""*** These values will be removed {values_to_eliminate} ***""")
 
-   result_frame = df[df["time_difference"]>args.gap][["line_number","time_difference","longer_gap","line"]]
+    result_frame = df[df["time_difference"] > args.gap][
+        ["line_number", "time_difference", "longer_gap", "line"]
+    ]
 
-   result_frame2 = result_frame[result_frame['time_difference']!=values_to_eliminate][["line_number","time_difference","longer_gap","line"]]
+    result_frame2 = result_frame[
+        result_frame["time_difference"] != values_to_eliminate
+    ][["line_number", "time_difference", "longer_gap", "line"]]
 
-   logger.info(f"""\n{result_frame2.to_markdown(index=False)}""")
+    logger.info(f"""\n{result_frame2.to_markdown(index=False)}""")
 
-   logger.info(f"""\n{result_frame.to_markdown(index=False)}""")
+    logger.info(f"""\n{result_frame.to_markdown(index=False)}""")
 
-   if args.output_format.lower() == "markdown":
-       print(f"""\n{result_frame.to_markdown(index=False)}""")
-   elif args.output_format.lower() == "csv":
-       print(f"""\n{result_frame.to_csv(index=False)}""")
-   else:
-       print(f"""\n{result_frame.to_string(index=False)}""")
+    if args.output_format.lower() == "markdown":
+        print(f"""\n{result_frame.to_markdown(index=False)}""")
+    elif args.output_format.lower() == "csv":
+        print(f"""\n{result_frame.to_csv(index=False)}""")
+    else:
+        print(f"""\n{result_frame.to_string(index=False)}""")
+
 
 if __name__ == "__main__":
-    """ This is executed when run from the command line """
+    """This is executed when run from the command line"""
 
     parser = argparse.ArgumentParser()
     parser.add_argument("filename", help="a csv to chomp.")
-    parser.add_argument("column_name", help="the column name of the date to evaluate" )
+    parser.add_argument("column_name", help="the column name of the date to evaluate")
     parser.add_argument("-f", "--flag", action="store_true", default=False)
-    parser.add_argument("-g", "--gap", action="store", dest="gap", type=int,default=300)
-    parser.add_argument("-t", "--tolerance", action="store", dest="tolerance", type=int,default=1)
+    parser.add_argument(
+        "-g", "--gap", action="store", dest="gap", type=int, default=300
+    )
+    parser.add_argument(
+        "-t", "--tolerance", action="store", dest="tolerance", type=int, default=1
+    )
 
     # Optional verbosity counter (eg. -v, -vv, -vvv, etc.)
     parser.add_argument(
-        "-v",
-        "--verbose",
-        action="count",
-        default=0,
-        help="Verbosity (-v, -vv, etc)")
+        "-v", "--verbose", action="count", default=0, help="Verbosity (-v, -vv, etc)"
+    )
 
     # Specify output of "--version"
     parser.add_argument(
         "--version",
         action="version",
-        version="%(prog)s (version {version})".format(version=__version__))
+        version="%(prog)s (version {version})".format(version=__version__),
+    )
 
     parser.add_argument(
         "--output-format",
@@ -98,7 +106,3 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     main(args)
-
-
-
-
