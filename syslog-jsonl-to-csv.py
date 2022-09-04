a902f1baf78d1b60921cdd614cec956074346c14
David Morris <gm3dmo@github.com>
2022-09-04 12:45:50 +0100

    Adding a very basic working version.

--- syslog-jsonl-to-csv.py
+++ syslog-jsonl-to-csv.py
@@ -107,11 +107,7 @@ def main(args):
                     continue
                 line = line.rstrip()
                 line_dict = {}
-                #logger.debug(f"""Processing: {line_number} __ {line} __""")
                 date, remains_of_line = line[:split_at_column], line[split_at_column:]
-                #logger.debug(
-                #    f"""line number: {line_number}: remains_of_line is of type: {type(remains_of_line)}"""
-                #)
                 r = remains_of_line
                 logger.debug(f"""type r: {type(r)}""")
                 w = r.lstrip(" ")
@@ -121,7 +117,9 @@ def main(args):
                     hostname = z[0]
                     d = lc.split_daemon(z[1])
                     daemon = d[0]
-                    logger.debug(f"""line number: {line_number}: z: ({z[2]})""")
+                    json_msg = json.loads(z[2])
+                    logger.debug(f"""line number: {line_number}: \n\n\n json_msg: ({json_msg})\n\n\n""")
+                    
                 else:
                     logger.warning(
                         f"squib: line {line_number} does not have host/daemon portion."
