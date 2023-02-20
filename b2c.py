18addf4032dafcf2bff5a696aa5d8e8dc7744d63
David Morris <gm3dmo@github.com>
2023-02-20 10:04:13 +0000

    Adding b2c.py script.

--- b2c.py
+++ b2c.py
@@ -10,11 +10,16 @@ import logging.config
 import argparse
 import datetime
 import pathlib
+import subprocess
 from pathlib import Path
 import log2csv as lc
 from pathlib import Path
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
+
+def check_db_exists():
+    return True
 
 
 def main(args):
@@ -50,38 +55,56 @@ def main(args):
         ".mode csv",
         ".separator ','",
     ]
+
     args.syslog_files = []
 
-    bin_dir = "syslog-to-csv"
+    #bin_dir = "syslog-to-csv"
+    bin_dir = "."
+
+
 
     (
         files_to_convert,
         sqlite_db_chunk,
         syslog_files,
     ) = lc.create_list_of_files_to_convert(args)
+    
+    
+    MACHINE_RUNNING = True
 
-    with open("phase-1-split-syslog-to-daemons.txt", "w") as f:
+    while MACHINE_RUNNING == True:
+        print(f"""Phase 1: Split syslog files out to a file per daemon""")
         for line in syslog_files:
-            f.writelines(
-                f"""{args.python_interpreter} {bin_dir}/{splitter} {line} --sankey\n"""
-            )
+                cmd = f"""{args.python_interpreter} {splitter} {line}"""
+                try:
+                    subprocess.run([cmd], check=True, shell=True)
+                except subprocess.CalledProcessError as err:
+                    print('ERROR:', err)
 
-    # Now that syslog has been split into a file per daemon, we want to
-    # include files like babeld.log for conversion to csv. Go round again
-    # and regenerate the list of files to convert:
-    (
-        files_to_convert,
-        sqlite_db_chunk,
-        syslog_files,
-    ) = lc.create_list_of_files_to_convert(args)
+            
+        print(f"""Converting: {len(files_to_convert)}""")
+        print(f"""Converting: {files_to_convert}""")
+        # Now that syslog has been split into a file per daemon, we want to
+        # include files like babeld.log for conversion to csv. Go round again
+        # and regenerate the list of files to convert:
+        (
+            files_to_convert,
+            sqlite_db_chunk,
+            syslog_files,
+        ) = lc.create_list_of_files_to_convert(args)
 
-    with open("phase-2-convert-to-csv.txt", "w") as f:
-        for line in files_to_convert:
-            f.writelines(f"{line}\n")
+        print(f"""Phase 2a: Generate Sankey Information based on file size""")
+        print(f"""Converting: {len(files_to_convert)}""")
+        print(f"""Converting: {files_to_convert}""")
+        print(f"""Phase 2b: Convert log files to CSV""")
+        print(f"""\n""")
+        
 
-    with open("phase-3-create-sqlitedb.txt", "w") as f:
-        for line in sqlite_db_chunk:
-            f.writelines(f"{line}\n")
+        print(f"""Phase 3: Generate sqlite database from csv files""")
+        print(f"""SQLITE: {sqlite_db_chunk}""")
+ 
+        if check_db_exists() == True:
+            MACHINE_RUNNING = False
 
 
 if __name__ == "__main__":
@@ -100,7 +123,7 @@ if __name__ == "__main__":
         "--python-interpreter",
         action="store",
         dest="python_interpreter",
-        default="pypy3",
+        default="python3",
         help="defines the python interpreter used: --python-interpreter /usr/bin/python",
     )
     parser.add_argument(
