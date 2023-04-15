9e085ad97c637c1b2e4499be8c9ed32d580a7fca
David Morris <gm3dmo@github.com>
2023-04-15 11:13:00 +0100

    adding log levels.

--- queries/babeld/babeld-001-ldevel-by-hour.sql
+++ queries/babeld/babeld-001-ldevel-by-hour.sql
@@ -1,8 +1,9 @@
 .mode columns
-.width 60
+.width 20 20 20 20
 .headers on
 .timer on
 SELECT strftime('%Y-%m-%d %H:00:00', now) as hour, 
-      sum(case when message = '' then 1 else 0 end) as column1,
-      sum(case when message = '' then 1 else 0 end) as column2 
+      sum(case when message = 'INFO' then 1 else 0 end) as info,
+      sum(case when message = 'WARN' then 1 else 0 end) as error,
+      sum(case when message = 'ERROR' then 1 else 0 end) as warn 
 FROM babeld GROUP BY hour ORDER BY hour;
