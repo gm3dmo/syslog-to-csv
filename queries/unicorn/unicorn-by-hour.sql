a1d30adf6e77c6be3de749708b4a94a3670854d2
David Morris <gm3dmo@github.com>
2023-04-15 18:30:03 +0100

    Adding unicorn query.

---
+++ queries/unicorn/unicorn-by-hour.sql
@@ -0,0 +1,14 @@
+.mode columns
+.width 20 20 20 20
+.headers on
+.timer on
+SELECT strftime('%Y-%m-%d %H:00:00', time) as timeframe,
+      sum(case when status like '20%' then 1 else 0 end) as status_20x,
+      sum(case when status like '30%' then 1 else 0 end) as status_30x,
+      sum(case when status = '403' then 1 else 0 end) as status_403,
+      sum(case when status = '404' then 1 else 0 end) as status_404,
+      sum(case when status = '406' then 1 else 0 end) as status_406,
+      sum(case when status = '409' then 1 else 0 end) as status_409,
+      sum(case when status = '422' then 1 else 0 end) as status_422,
+      sum(case when status like '50%' then 1 else 0 end) as status_50x
+FROM unicorn GROUP BY timeframe ORDER BY timeframe
