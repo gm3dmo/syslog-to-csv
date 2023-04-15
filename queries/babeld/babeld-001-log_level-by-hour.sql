9e06323870502a75cf290813dcd2e717d4b74a7f
David Morris <gm3dmo@github.com>
2023-04-15 14:20:21 +0100

    adding babeld hourly msg summary.

--- queries/babeld/babeld-001-log_level-by-hour.sql
+++ queries/babeld/babeld-001-log_level-by-hour.sql
@@ -2,8 +2,8 @@
 .width 20 20 20 20
 .headers on
 .timer on
-SELECT strftime('%Y-%m-%d %H:00:00', now) as hour, 
-      sum(case when message = 'INFO' then 1 else 0 end) as info,
-      sum(case when message = 'WARN' then 1 else 0 end) as error,
-      sum(case when message = 'ERROR' then 1 else 0 end) as warn 
+SELECT strftime('%Y-%m-%d %H:00:00', ts) as hour, 
+      sum(case when log_level = 'INFO' then 1 else 0 end) as info,
+      sum(case when log_level = 'WARN' then 1 else 0 end) as error,
+      sum(case when log_level = 'ERROR' then 1 else 0 end) as warn 
 FROM babeld GROUP BY hour ORDER BY hour;
---
+++ queries/babeld/babeld-001-msg-by-hour.sql
@@ -0,0 +1,13 @@
+.mode columns
+.width 20 20 20 20
+.headers on
+.timer on
+SELECT strftime('%Y-%m-%d %H:00:00', ts) as hour, 
+      sum(case when msg like 'pubkey probe denied%' then 1 else 0 end) as pubkey_probe_denied,
+      sum(case when msg like 'http op doen: (-1)%%' then 1 else 0 end) as http_op_done_minus1,
+      sum(case when msg like 'failed to verify pubkey:%' then 1 else 0 end) as pubkey_failed_verify,
+      sum(case when msg like 'repo auth check failed:%' then 1 else 0 end) as repo_auth_check_failed,
+      sum(case when msg like 'denying auth for non-git user%' then 1 else 0 end) as deny_auth_non_git_user,
+      sum(case when msg like 'http op done: (403)%' then 1 else 0 end) as http_op_done_403,
+      sum(case when msg like 'Timed out reading request body from client%' then 1 else 0 end) as tmout_read_req_body_from_client
+FROM babeld GROUP BY hour ORDER BY hour;
