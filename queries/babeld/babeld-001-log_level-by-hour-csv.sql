c5fffe4afe447e718a14199e77c3a365f3afdd0e
David Morris <gm3dmo@github.com>
2023-04-15 11:18:26 +0100

    Adding csv version.

---
+++ queries/babeld/babeld-001-log_level-by-hour-csv.sql
@@ -0,0 +1,7 @@
+.mode csv
+.headers on
+SELECT strftime('%Y-%m-%d %H:00:00', ts) as hour, 
+      sum(case when log_level = 'INFO' then 1 else 0 end) as info,
+      sum(case when log_level = 'WARN' then 1 else 0 end) as error,
+      sum(case when log_level = 'ERROR' then 1 else 0 end) as warn 
+FROM babeld GROUP BY hour ORDER BY hour;
