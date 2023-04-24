6783902da4734e25ed27de992bbdd3f50519fa73
David Morris <gm3dmo@github.com>
2023-04-24 17:31:27 +0100

    Update auth-success-fail-hour-saml.sql

--- queries/auth/auth-success-fail-hour-saml.sql
+++ queries/auth/auth-success-fail-hour-saml.sql
@@ -10,4 +10,4 @@ SELECT strftime('%Y-%m-%dT%H:00:00', auth.now) as timeframe,
       sum(case when message = 'Authentication failure' then 1 else 0 end) as auth_failure,
       sum(case when message = 'Password authentication is not supported by SAML authentication' then 1 else 0 end) as passwd_auth_not_supported_by_saml,
       sum(case when message like 'Unable to create the user because username is too long%' then 1 else 0 end) as username_too_long
-FROM auth GROUP BY hour ORDER BY timeframe;
+FROM auth GROUP BY timeframe ORDER BY timeframe;
