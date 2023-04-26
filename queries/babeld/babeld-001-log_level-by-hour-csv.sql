3104e7c5ef18651fee5798259eebf46ff4c77dd3
David Morris <gm3dmo@github.com>
2023-04-26 22:56:14 +0100

    Update babeld-001-log_level-by-hour-csv.sql

--- queries/babeld/babeld-001-log_level-by-hour-csv.sql
+++ queries/babeld/babeld-001-log_level-by-hour-csv.sql
@@ -1,7 +1,7 @@
 .mode csv
 .headers on
-SELECT strftime('%Y-%m-%d %H:00:00', ts) as hour, 
+SELECT strftime('%Y-%m-%d %H:00:00', ts) as timeframe, 
       sum(case when log_level = 'INFO' then 1 else 0 end) as info,
       sum(case when log_level = 'WARN' then 1 else 0 end) as error,
       sum(case when log_level = 'ERROR' then 1 else 0 end) as warn 
-FROM babeld GROUP BY hour ORDER BY hour;
+FROM babeld GROUP BY timeframe ORDER BY timeframe;
