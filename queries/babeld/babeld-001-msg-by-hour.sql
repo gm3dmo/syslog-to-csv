6b4a0743276ca80201f162a03d50cb4de0a56d1c
David Morris <gm3dmo@github.com>
2023-04-26 22:57:56 +0100

    Update babeld-001-msg-by-hour.sql

--- queries/babeld/babeld-001-msg-by-hour.sql
+++ queries/babeld/babeld-001-msg-by-hour.sql
@@ -1,8 +1,8 @@
-.mode columns
+.mode csv
 .width 20 20 20 20
 .headers on
 .timer on
-SELECT strftime('%Y-%m-%d %H:00:00', ts) as hour, 
+SELECT strftime('%Y-%m-%d %H:00:00', ts) as timeframe, 
       sum(case when msg like 'pubkey probe denied%' then 1 else 0 end) as pubkey_probe_denied,
       sum(case when msg like 'http op done: (-1)%%' then 1 else 0 end) as http_op_done_minus1,
       sum(case when msg like 'failed to verify pubkey:%' then 1 else 0 end) as pubkey_failed_verify,
@@ -10,4 +10,4 @@ SELECT strftime('%Y-%m-%d %H:00:00', ts) as hour,
       sum(case when msg like 'denying auth for non-git user%' then 1 else 0 end) as deny_auth_non_git_user,
       sum(case when msg like 'http op done: (403)%' then 1 else 0 end) as http_op_done_403,
       sum(case when msg like 'Timed out reading request body from client%' then 1 else 0 end) as tmout_read_req_body_from_client
-FROM babeld GROUP BY hour ORDER BY hour;
+FROM babeld GROUP BY hour ORDER BY timeframe;
