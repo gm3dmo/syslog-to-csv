ca1f6d87619ad9817c0e635b8b3383353f0a2019
David Morris <gm3dmo@github.com>
2023-04-28 18:20:02 +0100

    add bcolors class

--- log2csv.py
+++ log2csv.py
@@ -16,6 +16,16 @@ import unicodedata
 
 logger = logging.getLogger("log2csv")
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 def is_pypy3():
     return shutil.which("pypy3")
