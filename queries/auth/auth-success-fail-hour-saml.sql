7e25fe5b70fe0a5c6695a0758956ad30e1524759
David Morris <gm3dmo@github.com>
2023-05-20 11:09:32 +0100

    adding new queries

--- queries/auth/auth-success-fail-hour-saml.sql
+++ queries/auth/auth-success-fail-hour-saml.sql
@@ -1,8 +1,6 @@
 .mode csv
 .headers on
-.timer on
-.width 20 0 0 0
-SELECT strftime('%Y-%m-%dT%H:00:00', auth.now) as timeframe, 
+SELECT strftime('%Y-%m-%dT%H:%M:%S', datetime(strftime('%s', now) / 3600 * 3600, 'unixepoch')) AS timeframe,
       sum(case when message = 'Authentication success via token' then 1 else 0 end) as auth_success_via_token,
       sum(case when message = 'Authentication failure via token' then 1 else 0 end) as auth_failure_via_token,
       sum(case when message = 'Your account has been suspended. via token' then 1 else 0 end) as account_suspended_via_token,
@@ -10,4 +8,6 @@ SELECT strftime('%Y-%m-%dT%H:00:00', auth.now) as timeframe,
       sum(case when message = 'Authentication failure' then 1 else 0 end) as auth_failure,
       sum(case when message = 'Password authentication is not supported by SAML authentication' then 1 else 0 end) as passwd_auth_not_supported_by_saml,
       sum(case when message like 'Unable to create the user because username is too long%' then 1 else 0 end) as username_too_long
-FROM auth GROUP BY timeframe ORDER BY timeframe;
+FROM auth 
+GROUP BY timeframe 
+ORDER BY timeframe asc;
