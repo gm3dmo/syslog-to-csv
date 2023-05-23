23fd45042946e33c0dd746c5b209956f069c5fe2
David Morris <gm3dmo@github.com>
2023-05-23 08:48:25 +0100

    update creason query

--- queries/babeld/babeld-creason-bad-command-by-user-repo.sql
+++ queries/babeld/babeld-creason-bad-command-by-user-repo.sql
@@ -1,10 +1,18 @@
-### Which users/repos/banner  are "bad command" messages coming from in the babeld.log?
+/*
+### Which user/repo/banner  are "bad command" messages coming from in the babeld.log?
+*/
 
 .headers on
 .mode column
-.width 20 30 30 
-select creason, user agent, repo, banner, count(creason) as count_of_bad_command from babeld 
+.width 20 30 20
+select creason, user, msg, log_level, repo, banner, count(creason) as count_of_bad_command from babeld 
 where creason like 'bad command' 
 group by creason, user_agent, repo 
 order by count_of_bad_command desc 
 limit 50;
+
+/* show timespan of babeld.log */
+SELECT  min(ts) as "first_record", 
+        max(ts) as "last_record",  
+        printf("%.2f", JULIANDAY(max(ts)) - JULIANDAY(min(ts))) AS "babeld log span (days)" 
+from babeld where ts != '';
