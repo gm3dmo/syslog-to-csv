dbcd44c74b5a5a3b6364a0ca2619bec07b14642f
David Morris <gm3dmo@github.com>
2023-05-24 13:37:20 +0100

    adding user unicorn query

---
+++ queries/unicorn/unicorn-request-category-api-current_user.sql
@@ -0,0 +1,11 @@
+.mode csv
+.timer on
+.headers on
+SELECT 
+       strftime('%Y-%m-%dT%H:%M:%S', datetime(strftime('%s', now) / 300 * 300, 'unixepoch')) AS timeframe,
+       count(current_user) as count_of_current_user,
+       current_user
+FROM unicorn
+WHERE request_category = "api"
+GROUP BY timeframe 
+ORDER BY timeframe;
--- queries/unicorn/when-who-500-status.sql
+++ queries/unicorn/when-who-500-status.sql
@@ -3,7 +3,6 @@
 SELECT strftime('%Y-%m-%dT%H:%M:%S', datetime(strftime('%s', now) / 300 * 300, 'unixepoch')) AS timeframe,
        current_user,
        count(*) as count_of_errors,
-       elapsed
 FROM unicorn where status like '5%' 
 GROUP BY timeframe, route 
 ORDER BY timeframe;
