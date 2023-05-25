e06797f7afa588bb3a01b859c418f9bfda918261
David Morris <gm3dmo@github.com>
2023-05-25 09:48:40 +0100

    fix query

--- queries/unicorn/when-who-500-status.sql
+++ queries/unicorn/when-who-500-status.sql
@@ -1,9 +1,11 @@
+
+/* Extract users who had a 5xx error in unicorn. Broken down into timeframes */
+
 .mode csv
 .headers on
 SELECT strftime('%Y-%m-%dT%H:%M:%S', datetime(strftime('%s', now) / 300 * 300, 'unixepoch')) AS timeframe,
        current_user,
-       count(*) as count_of_errors,
+       count(*) as count_of_user
 FROM unicorn where status like '5%' 
-GROUP BY timeframe, route 
+GROUP BY timeframe 
 ORDER BY timeframe;
-
