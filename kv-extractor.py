50b5a60f8c53ee7d45330da024d173ffa85287c0
David Morris <gm3dmo@github.com>
2024-01-16 14:43:20 +0000

    Adding kv-extractor with a data class

--- kv-extractor.py
+++ kv-extractor.py
@@ -3,7 +3,7 @@
 An experiment in extracting key value pairs from files.
 """
 
-__author__ = "David Morris @gm3dmo"
+__author__ = "David Morris gm3dmo@gmail.com"
 __version__ = "0.1.0"
 
 import sys
@@ -14,13 +14,17 @@ import json
 
 from dataclasses import dataclass
 
-@dataclass
-class Report:
-    lines_total: int = 0
-    lines_processed: int = 0
-    lines_with_errors: int = 0
 
 logger = logging.getLogger(__name__)
+
+
+@dataclass
+class Report:
+    # Report on the number of lines processed/failed etc.
+    lines_total: int = 0
+    lines_parsed: int = 0
+    lines_with_read_failed: int = 0
+    lines_with_parse_failed: int = 0
 
 
 def parse_kv(text):
@@ -35,31 +39,42 @@ def parse_kv(text):
             logging.debug(f"""token: {token}""")
             key, value = token.split("=", 1)  # Split at the first equal sign only
             result[key] = value
+        else:
+            logging.debug(f"""token: {token} has no = sign to split on""")
 
     return result
 
 
-def main(args):
-    """ Main entry point of the app """
+def read_file_line_by_line(filename):
+    try:
+        with open(filename, 'r') as file:
+            for line_number, line in enumerate(file, start=1):
+                yield line_number, line, None
+    except Exception as e:
+        yield line_number, None, str(e)
 
+
+def main(args):
     log_level = args.log_level.upper()
     logging.basicConfig(level=log_level)
     logger = logging.getLogger(__name__)
 
-    log_entry = sys.stdin.read()
-
-    lines = log_entry.splitlines()
-
     report = Report()
-
     results = {}
-    for i, line in enumerate(lines):
+
+    for  (line_number, line, exception) in read_file_line_by_line(args.kv_input_file):
+        if line is None:
+            logger.warning(f"failed to read {args.kv_input_file} at line:{line_number}")
+            report.lines_with_read_failed += 1
+            continue    
         report.lines_total+= 1
         try:
-           r = parse_kv(log_entry)
+           r = parse_kv(line)
+           report.lines_parsed += 1
            results.update(r)
         except Exception as e:
-           logger.error(f"Failed to parse line {i}: {e}")
+            logger.warning(f"failed to parse the file {args.kv_input_file} at line:{line_number}: {e}")
+            report.lines_with_parse_failures += 1
 
     logger.debug(f"""keys: {results.keys()}""")
     logger.debug(f"""length keys: {len(results.keys())}""")
@@ -69,10 +84,10 @@ def main(args):
     print("------")
     print(report)
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-e", "--extension", action="store", dest="extension", default="c")
-
+    parser.add_argument('kv_input_file', help='Path to the log file the script should process.')
     parser.add_argument(
         "-l",
         "--log-level",
