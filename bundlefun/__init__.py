730f765d86657fa4fb7d29ea8accf6596991d9cc
David Morris <gm3dmo@github.com>
2024-01-31 11:52:45 +0000

    Update __init__.py

--- bundlefun/__init__.py
+++ bundlefun/__init__.py
@@ -1,1 +1,1 @@
-from .bundlefun import Report, read_file_line_by_line, parse_kv, timeit, get_filesize, sizer, get_wanted_kv_headers, open_file_handle, parse_kv_pairs_two, is_pypy3, create_list_of_syslog_files_to_split, create_list_of_files_to_convert_to_csv
+from .bundlefun import Report, read_file_line_by_line, parse_kv, timeit, get_filesize, sizer, get_wanted_kv_headers, open_file_handle, parse_kv_pairs_two, is_pypy3, create_list_of_syslog_files_to_split, create_list_of_files_to_convert_to_csv, slugify
