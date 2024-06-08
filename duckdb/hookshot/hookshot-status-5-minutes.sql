d6c076b0c1a38a9a15113c73f36902877aeeac70
David Morris <gm3dmo@github.com>
2024-06-08 17:05:47 +0100

    Update hookshot-status-5-minutes.sql

--- duckdb/hookshot/hookshot-status-5-minutes.sql
+++ duckdb/hookshot/hookshot-status-5-minutes.sql
@@ -2,7 +2,7 @@
 .mode csv
 .headers on
 SELECT 
-  date_trunc('minute', CAST("Timestamp" AS TIMESTAMP)) - INTERVAL '1 minute' * (EXTRACT(MINUTE FROM CAST("Timestamp" AS TIMESTAMP)) % 10) AS timeframe,
+  date_trunc('minute', CAST("Timestamp" AS TIMESTAMP)) - INTERVAL '1 minute' * (EXTRACT(MINUTE FROM CAST("Timestamp" AS TIMESTAMP)) % 5) AS timeframe,
   status,
   COUNT(*) AS count
 FROM hookshot
