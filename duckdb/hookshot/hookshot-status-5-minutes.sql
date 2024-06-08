81fd14a71fc0e9e8c8c324228420958d1eaa87cf
David Morris <gm3dmo@github.com>
2024-06-08 14:05:43 +0100

    Create hookshot-status-5-minutes.sql

---
+++ duckdb/hookshot/hookshot-status-5-minutes.sql
@@ -0,0 +1,10 @@
+
+.mode csv
+.headers on
+SELECT 
+  date_trunc('minute', CAST("Timestamp" AS TIMESTAMP)) - INTERVAL '1 minute' * (EXTRACT(MINUTE FROM CAST("Timestamp" AS TIMESTAMP)) % 10) AS timeframe,
+  status,
+  COUNT(*) AS count
+FROM hookshot
+GROUP BY timeframe, status
+ORDER BY timeframe, status;
