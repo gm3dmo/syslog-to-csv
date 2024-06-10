96d3e1208c058d52e427fdfbaada1c88a4f694a6
David Morris <gm3dmo@github.com>
2024-06-10 14:43:53 +0100

    Create spokesd-timespan.sql

---
+++ duckdb/spokesd/spokesd-timespan.sql
@@ -0,0 +1,7 @@
+.mode csv
+.headers on
+SELECT 
+      MIN(CAST(Timestamp AS TIMESTAMP)) AS min_date, 
+      MAX(CAST(Timestamp AS TIMESTAMP)) AS max_date, 
+      AGE(MAX(CAST(Timestamp AS TIMESTAMP)), MIN(CAST(Timestamp AS TIMESTAMP))) AS timespan
+  FROM spokesd;
