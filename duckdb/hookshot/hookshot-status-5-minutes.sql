e8a510b8a7e9d35a3d0562848869305deb89f335
David Morris <gm3dmo@github.com>
2024-06-13 12:21:56 +0100

    Update hookshot-status-5-minutes.sql

--- duckdb/hookshot/hookshot-status-5-minutes.sql
+++ duckdb/hookshot/hookshot-status-5-minutes.sql
@@ -5,6 +5,6 @@ SELECT
   date_trunc('minute', CAST("Timestamp" AS TIMESTAMP)) - INTERVAL '1 minute' * (EXTRACT(MINUTE FROM CAST("Timestamp" AS TIMESTAMP)) % 5) AS timeframe,
   status,
   COUNT(*) AS count
-FROM hookshot
+FROM hookshot where status is not null
 GROUP BY timeframe, status
 ORDER BY timeframe, status;
