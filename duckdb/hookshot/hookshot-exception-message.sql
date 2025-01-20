af6b6d3a01e6ef499e81439e0ed3110d96809798
David Morris <gm3dmo@github.com>
2025-01-20 15:59:11 +0000

    Update hookshot-exception-message.sql

--- duckdb/hookshot/hookshot-exception-message.sql
+++ duckdb/hookshot/hookshot-exception-message.sql
@@ -2,8 +2,8 @@
 .headers on
 SELECT 
   date_trunc('minute', CAST("Timestamp" AS TIMESTAMP)) - INTERVAL '1 minute' * (EXTRACT(MINUTE FROM CAST("Timestamp" AS TIMESTAMP)) % 5) AS timeframe,
-  exception.message,
+  hookshot.exception.message,
   COUNT(*) AS count
-FROM hookshot where exception.message is not null
-GROUP BY timeframe, exception.message
-ORDER BY timeframe, exception.message;
+FROM hookshot where hookshot.exception.message is not null
+GROUP BY timeframe, hookshot.exception.message
+ORDER BY timeframe, hookshot.exception.message;
