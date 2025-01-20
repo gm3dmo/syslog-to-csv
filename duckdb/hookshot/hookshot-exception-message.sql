ed0e446aa9406f80380e490aa5b04cabe84b02d1
David Morris <gm3dmo@github.com>
2025-01-20 15:57:50 +0000

    Create hookshot-exception-message.sql

---
+++ duckdb/hookshot/hookshot-exception-message.sql
@@ -0,0 +1,9 @@
+.mode csv
+.headers on
+SELECT 
+  date_trunc('minute', CAST("Timestamp" AS TIMESTAMP)) - INTERVAL '1 minute' * (EXTRACT(MINUTE FROM CAST("Timestamp" AS TIMESTAMP)) % 5) AS timeframe,
+  exception.message,
+  COUNT(*) AS count
+FROM hookshot where exception.message is not null
+GROUP BY timeframe, exception.message
+ORDER BY timeframe, exception.message;
