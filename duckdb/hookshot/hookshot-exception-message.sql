8a0fd864c4ac7460c559aa0390c9491e5a1d31d3
David Morris <gm3dmo@github.com>
2025-02-08 16:45:59 +0000

    Update hookshot-exception-message.sql

--- duckdb/hookshot/hookshot-exception-message.sql
+++ duckdb/hookshot/hookshot-exception-message.sql
@@ -1,9 +1,7 @@
-.mode csv
-.headers on
-SELECT 
-  date_trunc('minute', CAST("Timestamp" AS TIMESTAMP)) - INTERVAL '1 minute' * (EXTRACT(MINUTE FROM CAST("Timestamp" AS TIMESTAMP)) % 5) AS timeframe,
-  "exception.message",
-  COUNT(*) AS count
-FROM hookshot where "exception.message" is not null
-GROUP BY timeframe, "exception.message"
-ORDER BY timeframe, "exception.message";
+SELECT
+  date_trunc('minute', CAST("Timestamp" AS TIMESTAMP)) - INTERVAL '1 minute' * (EXTRACT(MINUTE FROM CAST("Timestamp" AS TIMESTAMP)) % 10) AS timeframe,
+  "http.status_code", "gh.repo.name_with_owner", "gh.request.api.route",  COUNT(*) AS count
+FROM unicorn
+WHERE "http.status_code" > 299
+GROUP BY timeframe, "gh.repo.name_with_owner", "http.status_code", "gh.request.api.route"
+ORDER BY timeframe, "http.status_code";
